ea318a011d854fb1907c269b0f21deaa6c0bbe28
Gerasimos Filippakos <filippakos.5@emea.teleperformance.com>
2020-02-11 14:27:05 +0200

    Added Unity Project, ground and walls generated!

---
+++ Simple Game/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private int mapWidth;
+
+    [SerializeField] private int platformLevels;
+
+    [SerializeField] private float platformLevelHeight;
+
+    [SerializeField] private GameObject floorPrefab;
+
+    [SerializeField] private GameObject wallPrefab;
+
+    [SerializeField] private bool UseStaticPlatforms;
+
+    [SerializeField] private GameObject[] staticPlatforms;
+
+    [SerializeField] private bool UseUpDownPlatforms;
+
+    [SerializeField] private GameObject[] upDownPlatforms;
+
+    [SerializeField] private bool UseLeftRightPlatforms;
+
+    [SerializeField] private GameObject[] leftRightPlatforms;
+
+    [SerializeField] private bool UseStaticTraps;
+
+    [SerializeField] private GameObject[] staticTraps;
+
+    [SerializeField] private bool UseEnemies;
+
+    [SerializeField] private GameObject[] enemies;
+}
---
+++ Simple Game/Assets/Scripts/PlatformInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInfo : MonoBehaviour
+{
+    public Vector3 platformStartingPoint;
+    public Vector3 platformEndingPoint;
+}
