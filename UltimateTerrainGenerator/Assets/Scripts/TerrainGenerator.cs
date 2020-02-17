d54b1a8534e35b651f6487ca90095c3d0ac9e176
Gerasimos <13-12608@saeinstitute.edu>
2020-02-17 22:17:50 +0200

    Progress in terrain generation

--- UltimateTerrainGenerator/Assets/Scripts/TerrainGenerator.cs
+++ UltimateTerrainGenerator/Assets/Scripts/TerrainGenerator.cs
@@ -10,30 +10,10 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private Material GroundMaterial;
 
     /// <summary>
-    /// Ideally used for top-down view, this should be the top-left corner of the terrain.
-    /// </summary>
-    [SerializeField] private Transform TopLeftCorner;
-
-    /// <summary>
-    /// Ideally used for top-down view, this should be the top-right corner of the terrain.
-    /// </summary>
-    [SerializeField] private Transform TopRightCorner;
-
-    /// <summary>
-    /// Ideally used for top-down view, this should be the bottom-left corner of the terrain.
-    /// </summary>
-    [SerializeField] private Transform BottomLeftCorner;
-
-    /// <summary>
-    /// Ideally used for top-down view, this should be the bottom-right corner of the terrain.
-    /// </summary>
-    [SerializeField] private Transform BottomRightCorner;
-
-    /// <summary>
     /// The number of subdivisions the terrain will have. The higher this number is
     /// the more detailed the terrain will be, as well as the number of triangles.
     /// </summary>
-    [SerializeField] private int Smoothness;
+    [SerializeField] private int Subdivisions;
 
     /// <summary>
     /// The lowest Y the terrain can have.
--- UltimateTerrainGenerator/Assets/Scripts/TerrainGeneratorEditor.cs
+++ UltimateTerrainGenerator/Assets/Scripts/TerrainGeneratorEditor.cs
@@ -10,31 +10,31 @@ public class TerrainGeneratorEditor : Editor
     /// </summary>
     private SerializedProperty GroundMaterial;
 
-    /// <summary>
-    /// Ideally used for top-down view, this should be the top-left corner of the terrain.
-    /// </summary>
-    private SerializedProperty TopLeftCorner;
+    ///// <summary>
+    ///// Ideally used for top-down view, this should be the top-left corner of the terrain.
+    ///// </summary>
+    //private SerializedProperty TopLeftCorner;
 
-    /// <summary>
-    /// Ideally used for top-down view, this should be the top-right corner of the terrain.
-    /// </summary>
-    private SerializedProperty TopRightCorner;
+    ///// <summary>
+    ///// Ideally used for top-down view, this should be the top-right corner of the terrain.
+    ///// </summary>
+    //private SerializedProperty TopRightCorner;
 
-    /// <summary>
-    /// Ideally used for top-down view, this should be the bottom-left corner of the terrain.
-    /// </summary>
-    private SerializedProperty BottomLeftCorner;
+    ///// <summary>
+    ///// Ideally used for top-down view, this should be the bottom-left corner of the terrain.
+    ///// </summary>
+    //private SerializedProperty BottomLeftCorner;
 
-    /// <summary>
-    /// Ideally used for top-down view, this should be the bottom-right corner of the terrain.
-    /// </summary>
-    private SerializedProperty BottomRightCorner;
+    ///// <summary>
+    ///// Ideally used for top-down view, this should be the bottom-right corner of the terrain.
+    ///// </summary>
+    //private SerializedProperty BottomRightCorner;
 
     /// <summary>
     /// The number of subdivisions the terrain will have. The higher this number is
     /// the more detailed the terrain will be, as well as the number of triangles.
     /// </summary>
-    private SerializedProperty Smoothness;
+    private SerializedProperty Subdivisions;
 
     /// <summary>
     /// The lowest Y the terrain can have.
@@ -46,7 +46,7 @@ public class TerrainGeneratorEditor : Editor
     /// </summary>
     private SerializedProperty maxHeight;
 
-    bool cornerFoldout = true;
+    //bool cornerFoldout = true;
 
     private GameObject terrainGameObject;
 
@@ -60,15 +60,32 @@ public class TerrainGeneratorEditor : Editor
     {
         GroundMaterial = serializedObject.FindProperty("GroundMaterial");
 
-        TopLeftCorner = serializedObject.FindProperty("TopLeftCorner");
-        TopRightCorner = serializedObject.FindProperty("TopRightCorner");
-        BottomLeftCorner = serializedObject.FindProperty("BottomLeftCorner");
-        BottomRightCorner = serializedObject.FindProperty("BottomRightCorner");
+        //TopLeftCorner = serializedObject.FindProperty("TopLeftCorner");
+        //TopRightCorner = serializedObject.FindProperty("TopRightCorner");
+        //BottomLeftCorner = serializedObject.FindProperty("BottomLeftCorner");
+        //BottomRightCorner = serializedObject.FindProperty("BottomRightCorner");
 
-        Smoothness = serializedObject.FindProperty("Smoothness");
+        Subdivisions = serializedObject.FindProperty("Subdivisions");
 
         minHeight = serializedObject.FindProperty("minHeight");
         maxHeight = serializedObject.FindProperty("maxHeight");
+
+        if (terrainGameObject == null)
+        {
+            terrainGameObject = GameObject.Find("Ultimate Terrain");
+
+            if (!terrainGameObject)
+            {
+                terrainGameObject = new GameObject("Ultimate Terrain",
+                typeof(MeshFilter),
+                typeof(MeshRenderer),
+                typeof(MeshCollider));
+            }
+
+            meshFilter = terrainGameObject.GetComponent<MeshFilter>();
+            meshRenderer = terrainGameObject.GetComponent<MeshRenderer>();
+            meshCollider = terrainGameObject.GetComponent<MeshCollider>();
+        }
     }
 
     public override void OnInspectorGUI()
@@ -82,21 +99,7 @@ public class TerrainGeneratorEditor : Editor
 
         EditorGUILayout.Space();
 
-        cornerFoldout = EditorGUILayout.Foldout(cornerFoldout, "Corners", true);
-
-        if (cornerFoldout)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(TopLeftCorner, new GUIContent("Top Left"));
-            EditorGUILayout.PropertyField(TopRightCorner, new GUIContent("Top Right"));
-            EditorGUILayout.PropertyField(BottomLeftCorner, new GUIContent("Bottom Left"));
-            EditorGUILayout.PropertyField(BottomRightCorner, new GUIContent("Bottom Right"));
-            EditorGUI.indentLevel--;
-        }
-
-        EditorGUILayout.Space();
-
-        Smoothness.intValue = EditorGUILayout.DelayedIntField(new GUIContent("Smoothness"), Smoothness.intValue);
+        Subdivisions.intValue = EditorGUILayout.DelayedIntField(new GUIContent("Subdivisions"), Subdivisions.intValue);
 
         EditorGUILayout.Space();
 
@@ -105,7 +108,7 @@ public class TerrainGeneratorEditor : Editor
 
         EditorGUILayout.EndVertical();
 
-        EditorGUILayout.LabelField(new GUIContent("_____________________________"));
+        serializedObject.ApplyModifiedProperties();
 
         if (GUILayout.Button("Generate Terrain"))
         {
@@ -113,26 +116,68 @@ public class TerrainGeneratorEditor : Editor
         }
 
         EditorGUI.indentLevel--;
-        serializedObject.ApplyModifiedProperties();
     }
 
     private void GenerateTerrain()
     {
-        Debug.Log("Someday maybe, not yet for sure!");
+        mesh = new Mesh();
 
-        if (!terrainGameObject)
+        int tilesPerAxis = Subdivisions.intValue;
+        int vertsPerAxis = tilesPerAxis + 1;
+
+        int numVerts = vertsPerAxis * vertsPerAxis;
+        int numTris = tilesPerAxis * tilesPerAxis * 2;
+
+        float tileWidth = 2.0f / tilesPerAxis;
+
+        Vector3[] vertices = new Vector3[numVerts];
+
+        for (int ix = 0; ix <= vertsPerAxis - 1; ix++)
         {
-            terrainGameObject = new GameObject("Ultimate Terrain",
-                typeof(MeshFilter),
-                typeof(MeshRenderer),
-                typeof(MeshCollider));
+            for (int iy = 0; iy <= vertsPerAxis - 1; iy++)
+            {
+                float x = tileWidth * ix - 1;
+                float y = tileWidth * iy - 1;
 
-            meshFilter = terrainGameObject.GetComponent<MeshFilter>();
-            meshRenderer = terrainGameObject.GetComponent<MeshRenderer>();
-            meshCollider = terrainGameObject.GetComponent<MeshCollider>();
+                float h = Random.Range(minHeight.floatValue, maxHeight.floatValue);
+                //float h = Mathf.PerlinNoise(x, y) * height;
+
+                Vector3 vert = new Vector3(x, h, y);
+
+                int i = iy * vertsPerAxis + ix;
+                vertices[i] = vert;
+            }
         }
 
-        //TODO: Actually create the terrain with all the given variables!
-        Debug.Log("TODO: Create the terrain with all the given variables!");
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[numTris * 6];
+
+        for (int iy = 0; iy <= vertsPerAxis - 2; iy++)
+        {
+            for (int ix = 0; ix <= vertsPerAxis - 2; ix++)
+            {
+                int A = iy * vertsPerAxis + ix;
+                int B = iy * vertsPerAxis + ix + 1;
+                int C = (iy + 1) * vertsPerAxis + ix;
+                int D = (iy + 1) * vertsPerAxis + ix + 1;
+                int i = (iy * (vertsPerAxis - 1) + ix) * 6;
+
+                triangles[i + 0] = A;
+                triangles[i + 1] = C;
+                triangles[i + 2] = B;
+                triangles[i + 3] = B;
+                triangles[i + 4] = C;
+                triangles[i + 5] = D;
+            }
+        }
+
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+        meshRenderer.sharedMaterial = (Material)GroundMaterial.objectReferenceValue;
+        meshCollider.sharedMesh = mesh;
     }
 }
