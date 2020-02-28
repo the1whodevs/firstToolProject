005fb21d56ba27b55d6197e3ee2fa481230fe437
Gerasimos Filippakos <gerfil18-github@yahoo.com>
2020-02-28 17:40:24 +0200

    Moved Generator behaviour to Generator Script from Editor Script

--- UltimateTerrainGenerator/Assets/Ultimate Terrain Generator/Scripts/TerrainGenerator.cs
+++ UltimateTerrainGenerator/Assets/Ultimate Terrain Generator/Scripts/TerrainGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [ExecuteInEditMode]
 public class TerrainGenerator : MonoBehaviour
@@ -36,4 +37,233 @@ public class TerrainGenerator : MonoBehaviour
     [Range(0.1f, 5.0f)]
     [SerializeField] private float seed;
 
+    /// <summary>
+    /// If true, instead of making a mesh vertex by vertex, it positions and scales cubes on the 
+    /// calculated vertex positions.
+    /// </summary>
+    [SerializeField] private bool doMinecraft;
+
+    [SerializeField] private GameObject terrainGameObject;
+
+    private Vector3[] vertices;
+
+    private List<int> triList = new List<int>();
+
+    private int[] triangles;
+    private int trianglesMade;
+
+    private float xSpace;
+    private float zSpace;
+
+    private Mesh mesh;
+
+    //[SerializeField] private MeshFilter meshFilter;
+    //[SerializeField] private MeshRenderer meshRenderer;
+    //[SerializeField] private MeshCollider meshCollider;
+
+    public void GenerateTerrain(ref MeshFilter meshFilter, ref MeshRenderer meshRenderer, ref MeshCollider meshCollider, float div)
+    {
+        xSpace = (float)xSize / (Subdivisions * xSize);
+        zSpace = (float)zSize / (Subdivisions * zSize);
+
+
+        // First make the new vertex array
+        CreateVertices(div);
+
+        // Then create the needed triangles
+        CreateTriangles();
+
+        Debug.LogFormat("For {0} vertices, I made {1} triangles.", vertices.Length, trianglesMade);
+
+        mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        meshFilter.sharedMesh = mesh;
+        meshRenderer.sharedMaterial = GroundMaterial;
+        meshCollider.sharedMesh = mesh;
+
+        mesh.RecalculateNormals();
+    }
+
+    private void CreateVertices(float div)
+    {
+        int currentIndex = 0;
+
+        float xValue, zValue, y;
+
+        vertices = new Vector3[(xSize * Subdivisions + 1) * (zSize * Subdivisions + 1)];
+
+        for (int z = 0; z <= zSize * Subdivisions; z++)
+        {
+            for (int x = 0; x <= xSize * Subdivisions; x++)
+            {
+                xValue = x / (float)Subdivisions;
+                zValue = z / (float)Subdivisions;
+
+                y = Mathf.Clamp(Mathf.PerlinNoise(xValue + div, zValue + div) * 3.0f, 0.0f, maxHeight);
+
+                vertices[currentIndex] = new Vector3(xValue, y, zValue);
+
+                //// **** TEMP ****
+                //GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //g.transform.position = vertices[currentIndex];
+                //g.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+                //g.transform.SetParent(terrainGameObject.transform, true);
+                //g.name = currentIndex.ToString();
+                //// **** TEMP ****
+
+                currentIndex++;
+            }
+        }
+
+    }
+
+
+    private void CreateTriangles()
+    {
+        trianglesMade = 0;
+        triList.Clear();
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            // Returns true if a triangle is made. False if not.
+            // Used for customizing the code to the user's need.
+            bool BL = TryMakingBLTriangle(i);
+            bool TR = TryMakingTRTriangle(i);
+
+            #region Example Returned Bool Usage
+
+            /* An example of how the bool returned values could be used.
+                 * In this case, it generates "walls" around the terrain.
+                 */
+            //if (!BL || !TR)
+            //{
+            //    GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    g.name = vertices[i].ToString();
+            //    g.transform.localScale = new Vector3(xSpace, 100.0f, zSpace);
+            //    g.transform.position = vertices[i];
+            //}
+
+            #endregion
+        }
+
+        // Convert the triangle list to array.
+        triangles = new int[triList.Count];
+        triangles = triList.ToArray();
+    }
+
+    /// <summary>
+    /// Try making a Bottom-Left triangle with the vertice at the 'currentIndex'
+    /// being the start, positioned at the Bottom-Left of the triangle to be made.
+    /// </summary>
+    /// <param name="currentIndex"></param>
+    private bool TryMakingBLTriangle(int currentIndex)
+    {
+        // If we're at the last index, index + 1 throws IndexOutOfRange!
+        if (currentIndex >= vertices.Length - 1)
+        {
+            return false;
+        }
+
+        Vector3 currentVertex = vertices[currentIndex];
+        Vector3 nextVertex = vertices[currentIndex + 1];
+
+        int vertexAboveMeIndex = FindVertexAt(currentVertex.x, currentVertex.z + zSpace);
+
+        // For a BL triangle to be made:
+        // --> A vertex exists at the position (currentVertex.x, currentVertex.z + zSpace)
+        if (vertexAboveMeIndex == -1)
+        {
+            return false;
+        }
+
+        // --> The next vertex must have pos.z == to the current vertex
+        // --> The next vertex must have pos.x == currentVertex.x + xSpace
+        if (currentVertex.z != nextVertex.z)
+        {
+            return false;
+        }
+
+        float neededNextX = currentVertex.x + xSpace;
+        if (!Mathf.Approximately(nextVertex.x, neededNextX))
+        {
+            return false;
+        }
+
+        triList.Add(currentIndex);
+        triList.Add(vertexAboveMeIndex);
+        triList.Add(currentIndex + 1);
+
+        trianglesMade++;
+        return true;
+    }
+
+    /// <summary>
+    /// Try making a Top-Right triangle with the vertice at the 'currentIndex'
+    /// being the start, positioned at the Top-Right of the triangle to be made.
+    /// </summary>
+    /// <param name="currentIndex"></param>
+    private bool TryMakingTRTriangle(int currentIndex)
+    {
+        // If we're at 0, currentIndex - 1 throws IndexOutOfRange!
+        if (currentIndex == 0)
+        {
+            return false;
+        }
+
+        Vector3 currentVertex = vertices[currentIndex];
+        Vector3 previousVertex = vertices[currentIndex - 1];
+
+        int vertexBelowMeIndex = FindVertexAt(currentVertex.x, currentVertex.z - zSpace);
+
+        // For a TR triangle to be made:
+        // --> A vertex exists at the position (currentVertex.x, currentVertex.z - zSpace)
+        if (vertexBelowMeIndex == -1)
+        {
+            return false;
+        }
+
+        // --> The previous vertex must have pos.z == to the current vertex
+        // --> The previous vertex must have pos.x == currentVertex.x - xSpace
+        if (currentVertex.z != previousVertex.z)
+        {
+            return false;
+        }
+
+        float neededPreviousX = currentVertex.x - xSpace;
+        if (!Mathf.Approximately(previousVertex.x, neededPreviousX))
+        {
+            return false;
+        }
+
+        triList.Add(currentIndex);
+        triList.Add(vertexBelowMeIndex);
+        triList.Add(currentIndex - 1);
+
+        trianglesMade++;
+        return true;
+    }
+
+    /// <summary>
+    /// Returns the index of the first vertex found in the 'vertices' array,
+    /// with the given x and z position (y is ignored). If no vertex is found,
+    /// returns -1 instead.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
+    /// <returns></returns>
+    private int FindVertexAt(float x, float z)
+    {
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (Mathf.Approximately(vertices[i].x, x) && Mathf.Approximately(vertices[i].z, z))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
--- UltimateTerrainGenerator/Assets/Ultimate Terrain Generator/Scripts/TerrainGeneratorEditor.cs
+++ UltimateTerrainGenerator/Assets/Ultimate Terrain Generator/Scripts/TerrainGeneratorEditor.cs
@@ -37,27 +37,18 @@ public class TerrainGeneratorEditor : Editor
 
     private SerializedProperty seed;
 
-    float div;
-
-    private GameObject terrainGameObject;
-
-    private Vector3[] vertices;
-
-    private List<int> triList = new List<int>();
-    private int[] triangles;
-    private int trianglesMade;
-
-    private float xSpace;
-    private float zSpace;
-
-    private Mesh mesh;
+    private SerializedProperty terrainGameObject;
 
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
     private MeshCollider meshCollider;
 
+    private float div;
+
     void OnEnable()
     {
+        terrainGameObject = serializedObject.FindProperty("terrainGameObject");
+
         GroundMaterial = serializedObject.FindProperty("GroundMaterial");
 
         xSize = serializedObject.FindProperty("xSize");
@@ -70,21 +61,21 @@ public class TerrainGeneratorEditor : Editor
         // minHeight = serializedObject.FindProperty("minHeight");
         maxHeight = serializedObject.FindProperty("maxHeight");
 
-        if (terrainGameObject == null)
+        if (terrainGameObject.objectReferenceValue == null)
         {
-            terrainGameObject = GameObject.Find("Ultimate Terrain");
+            terrainGameObject.objectReferenceValue = GameObject.Find("Ultimate Terrain");
 
-            if (!terrainGameObject)
+            if (!terrainGameObject.objectReferenceValue)
             {
-                terrainGameObject = new GameObject("Ultimate Terrain",
+                terrainGameObject.objectReferenceValue = new GameObject("Ultimate Terrain",
                 typeof(MeshFilter),
                 typeof(MeshRenderer),
                 typeof(MeshCollider));
             }
 
-            meshFilter = terrainGameObject.GetComponent<MeshFilter>();
-            meshRenderer = terrainGameObject.GetComponent<MeshRenderer>();
-            meshCollider = terrainGameObject.GetComponent<MeshCollider>();
+            meshFilter = ((GameObject)terrainGameObject.objectReferenceValue).GetComponent<MeshFilter>();
+            meshRenderer = ((GameObject)terrainGameObject.objectReferenceValue).GetComponent<MeshRenderer>();
+            meshCollider = ((GameObject)terrainGameObject.objectReferenceValue).GetComponent<MeshCollider>();
         }
     }
 
@@ -123,7 +114,7 @@ public class TerrainGeneratorEditor : Editor
 
         if (GUILayout.Button("Generate Terrain"))
         {
-            GenerateTerrain();
+            ((TerrainGenerator)target).GenerateTerrain(ref meshFilter, ref meshRenderer, ref meshCollider, div);
         }
 
         EditorGUILayout.Separator();
@@ -138,212 +129,6 @@ public class TerrainGeneratorEditor : Editor
 
 
         EditorGUI.indentLevel--;
-    }
-
-    private void GenerateTerrain()
-    {
-        xSpace = (float)xSize.intValue / (Subdivisions.intValue * xSize.intValue);
-        zSpace = (float)zSize.intValue / (Subdivisions.intValue * zSize.intValue);
-
-
-        // First make the new vertex array
-        CreateVertices();
-
-        // Then create the needed triangles
-        CreateTriangles();
-
-        Debug.LogFormat("For {0} vertices, I made {1} triangles.", vertices.Length, trianglesMade);
-
-        mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-
-        meshFilter.sharedMesh = mesh;
-        meshRenderer.sharedMaterial = (Material)GroundMaterial.objectReferenceValue;
-        meshCollider.sharedMesh = mesh;
-
-        mesh.RecalculateNormals();
-    }
-
-    private void CreateVertices()
-    {
-        int currentIndex = 0;
-
-        float xValue, zValue, y;
-
-        vertices = new Vector3[(xSize.intValue * Subdivisions.intValue + 1) * (zSize.intValue * Subdivisions.intValue + 1)];
-
-        for (int z = 0; z <= zSize.intValue * Subdivisions.intValue; z++)
-        {
-            for (int x = 0; x <= xSize.intValue * Subdivisions.intValue; x++)
-            {
-                xValue = x / (float)Subdivisions.intValue;
-                zValue = z / (float)Subdivisions.intValue;
-
-                y = Mathf.Clamp(Mathf.PerlinNoise(xValue + div + vertices.Length, zValue + div + vertices.Length), 0.0f, maxHeight.floatValue);
-
-                vertices[currentIndex] = new Vector3(xValue, y, zValue);
-
-                //// **** TEMP ****
-                //GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //g.transform.position = vertices[currentIndex];
-                //g.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-                //g.transform.SetParent(terrainGameObject.transform, true);
-                //g.name = currentIndex.ToString();
-                //// **** TEMP ****
-
-                currentIndex++;
-            }
-        }
-
-    }
-
-
-    private void CreateTriangles()
-    {
-        trianglesMade = 0;
-        triList.Clear();
-
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            // Returns true if a triangle is made. False if not.
-            // Used for customizing the code to the user's need.
-            bool BL = TryMakingBLTriangle(i);
-            bool TR = TryMakingTRTriangle(i);
-
-            #region Example Returned Bool Usage
-
-            /* An example of how the bool returned values could be used.
-                 * In this case, it generates "walls" around the terrain.
-                 */
-            //if (!BL || !TR)
-            //{
-            //    GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //    g.name = vertices[i].ToString();
-            //    g.transform.localScale = new Vector3(xSpace, xSpace, zSpace);
-            //    g.transform.position = vertices[i];
-            //} 
-
-            #endregion
-        }
-
-        // Convert the triangle list to array.
-        triangles = new int[triList.Count];
-        triangles = triList.ToArray();
-    }
-
-    /// <summary>
-    /// Try making a Bottom-Left triangle with the vertice at the 'currentIndex'
-    /// being the start, positioned at the Bottom-Left of the triangle to be made.
-    /// </summary>
-    /// <param name="currentIndex"></param>
-    private bool TryMakingBLTriangle(int currentIndex)
-    {
-        // If we're at the last index, index + 1 throws IndexOutOfRange!
-        if (currentIndex >= vertices.Length - 1)
-        {
-            return false;
-        }
-
-        Vector3 currentVertex = vertices[currentIndex];
-        Vector3 nextVertex = vertices[currentIndex + 1];
-
-        int vertexAboveMeIndex = FindVertexAt(currentVertex.x, currentVertex.z + zSpace);
-
-        // For a BL triangle to be made:
-        // --> A vertex exists at the position (currentVertex.x, currentVertex.z + zSpace)
-        if (vertexAboveMeIndex == -1)
-        {
-            return false;
-        }
-
-        // --> The next vertex must have pos.z == to the current vertex
-        // --> The next vertex must have pos.x == currentVertex.x + xSpace
-        if (currentVertex.z != nextVertex.z)
-        {
-            return false;
-        }
-
-        float neededNextX = currentVertex.x + xSpace;
-        if (!Mathf.Approximately(nextVertex.x, neededNextX))
-        {
-            return false;
-        }
-
-        triList.Add(currentIndex);
-        triList.Add(vertexAboveMeIndex);
-        triList.Add(currentIndex + 1);
-
-        trianglesMade++;
-        return true;
-    }
-
-    /// <summary>
-    /// Try making a Top-Right triangle with the vertice at the 'currentIndex'
-    /// being the start, positioned at the Top-Right of the triangle to be made.
-    /// </summary>
-    /// <param name="currentIndex"></param>
-    private bool TryMakingTRTriangle(int currentIndex)
-    {
-        // If we're at 0, currentIndex - 1 throws IndexOutOfRange!
-        if (currentIndex == 0)
-        {
-            return false;
-        }
-
-        Vector3 currentVertex = vertices[currentIndex];
-        Vector3 previousVertex = vertices[currentIndex - 1];
-
-        int vertexBelowMeIndex = FindVertexAt(currentVertex.x, currentVertex.z - zSpace);
-
-        // For a TR triangle to be made:
-        // --> A vertex exists at the position (currentVertex.x, currentVertex.z - zSpace)
-        if (vertexBelowMeIndex == -1)
-        {
-            return false;
-        }
-
-        // --> The previous vertex must have pos.z == to the current vertex
-        // --> The previous vertex must have pos.x == currentVertex.x - xSpace
-        if (currentVertex.z != previousVertex.z)
-        {
-            return false;
-        }
-
-        float neededPreviousX = currentVertex.x - xSpace;
-        if (!Mathf.Approximately(previousVertex.x, neededPreviousX))
-        {
-            return false;
-        }
-
-        triList.Add(currentIndex);
-        triList.Add(vertexBelowMeIndex);
-        triList.Add(currentIndex - 1);
-
-        trianglesMade++;
-        return true;
-    }
-
-    /// <summary>
-    /// Returns the index of the first vertex found in the 'vertices' array,
-    /// with the given x and z position (y is ignored). If no vertex is found,
-    /// returns -1 instead.
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="z"></param>
-    /// <returns></returns>
-    private int FindVertexAt(float x, float z)
-    {
-
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            if (Mathf.Approximately(vertices[i].x, x) && Mathf.Approximately(vertices[i].z, z))
-            {
-                return i;
-            }
-        }
-
-        return -1;
     }
 
     public static class MeshSaverEditor
